e83d58320ddbd60385c3c6a34ad116f1f1eb851c
WhittSellers <44481407+WhittSellers@users.noreply.github.com>
2022-04-15 17:38:10 -0400

    continuing to debug vive tracker not working with OpenXR
    Added a couple new scripts and can now see that unity sees the vive tracker in its list of connected devices, but something is still not right because it transforms of the dragon staff are still not updating

--- XRDragonStaff_2020.3.28_HDRP/Assets/Whitt/Scripts/VFXManager.cs
+++ XRDragonStaff_2020.3.28_HDRP/Assets/Whitt/Scripts/VFXManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class VFXManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        GetVRDevices();
+    }
+
+    void GetVRDevices()
+    {
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+        }
     }
 }
